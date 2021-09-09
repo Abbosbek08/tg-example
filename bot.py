35b79e891467ed375bcaa71e0a10baceb2b586bd
Abbosbek08 <87572637+Abbosbek08@users.noreply.github.com>
2021-09-09 14:24:18 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+import telebot
+token="BOT TOKENI"
+bot=telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+  user=message.chat.first_name
+  text=f"Assalomu aleykum {user}"
+  bot.reply_to(message,text)
+bot.polling(none_stop=True)
